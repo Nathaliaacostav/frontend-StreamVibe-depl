d655f724d24cb406c47df320a43275e2a79d1777
Nathalia Acosta Vélez <nathalia285@gmail.com>
2024-05-31 12:04:11 -0500

    conexión con el backend

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  /* baseURL: import.meta.env.VITE_API_URL */
+  baseURL: "http://54.232.45.246:8000/"
 })
